cb98ee726ac9c9c25b43c128242f2a77e5215857
Kristjan Korjus <korjus@gmail.com>
2018-06-07 23:03:50 +0300

    to HTML

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 def test():
     result_string = ''
     for i in range(10):
-        result_string = result_string + 'Hello world!\n'
+        result_string = result_string + 'Hello world!<br>'
     return result_string
 
 if __name__ == '__main__':
