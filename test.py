853cc51c3edde2725720931b36988d6dfc45dec8
Kristjan Korjus <korjus@gmail.com>
2018-06-08 21:55:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 def test(a):
     result_string = ''
     for i in range(a):
-        result_string = result_string + 'Hello world!<br>'
+        result_string = result_string + '<img src="https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/11/enhanced/webdr11/anigif_enhanced-6690-1424363569-2.gif" alt="Smiley face" height="42" width="42"><br>'
     return result_string
 
 if __name__ == '__main__':
